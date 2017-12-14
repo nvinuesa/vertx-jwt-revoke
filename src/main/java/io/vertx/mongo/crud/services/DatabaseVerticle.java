08077dcdbe5184f2b3e51403fc17b6c5c395f2a2
underscorenico <vinuesa.nico@gmail.com>
2017-12-14 21:55:50 +0100

    fix: Add mongo dependency

--- src/main/java/io/vertx/mongo/crud/services/DatabaseVerticle.java
+++ src/main/java/io/vertx/mongo/crud/services/DatabaseVerticle.java
@@ -1,6 +1,15 @@
 package io.vertx.mongo.crud.services;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.mongo.MongoClient;
 
 public class DatabaseVerticle extends AbstractVerticle {
+
+  private MongoClient client;
+
+  @Override
+  public void start() throws Exception {
+
+    client = MongoClient.createShared(vertx, config());
+  }
 }
